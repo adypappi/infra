faa21fd89cb15deb99b9a2d17127e99822a78f95
timsli <infracloud@sysetele.com>
2018-04-04 03:28:08 +0200

    Create CreatePapiFSStructure.sh

---
+++ scripts/infra/CreatePapiFSStructure.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Script allowing to create the fs tree structure of papi cloud management plateform 
+mkdir -p /papi/devprj
+mkdir -p /papi/infraas
+mkdir -p /papi/infra/cntrs
+mkdir -p /papi/infra/vms
+mkdir -p /papi/infra/scripts
+mkdir -p /papi/infra/tools
+mkdir -p /papi/infra/docs
+mkdir -p /papi/tools/binary
+mkdir -p /papi/tools/src
+mkdir -p /papi/app
+mkdir -p /papi/appsrv
+mkdir -p /papi/docs
+mkdir -p /papi/appsrv
+mkdir -p /papi/dataset
+mkdir -p /papibackup
