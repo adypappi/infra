f204660c658cdaddf851ebbcc4d196b5fcd565a9
adimida <infracloud@sysetele.com>
2018-04-18 10:56:09 +0200

    ReAdd after minor modifications

--- scripts/CreateEnvironmentVariables.sh
+++ scripts/CreateEnvironmentVariables.sh
@@ -7,14 +7,18 @@
 # Create: 18/4/2018
 ######################################
 
+# Vim IS DEFAULT EDITOR IN ADIPAPPI.NET CONTEXT
+export VISUAL=vim
+export EDITOR="$VISUAL"
+
 # Adipappi environment variables used by server os
-http_proxy=http://pfwprxlnx1.adipappi.int:8989
-https_proxy=http://pfwprxlnx1.adipappi.int:8989
-no_proxy='127.0.0.1,localhost,adipappi.int'
+export http_proxy=http://pfwprxlnx1.adipappi.int:8989
+export https_proxy=http://pfwprxlnx1.adipappi.int:8989
+export no_proxy='127.0.0.1,localhost,adipappi.int'
 
 #Environment variables usefull for adipappi actions
-PAPI_HOME=/papi
-PAPI_SCRIPTS=/papi/scripts
-PAPI_INFRA=/papi/infra
-PAPI_INFRA_SCRIPTS=/papi/infra/scripts
-PAPI_SCRIPTS_LOGS_DIR=/papi/infra/scripts/logs
+export PAPI_HOME=/papi
+export PAPI_SCRIPTS=/papi/scripts
+export PAPI_INFRA=/papi/infra
+export PAPI_INFRA_SCRIPTS=/papi/infra/scripts
+export PAPI_SCRIPTS_LOGS_DIR=/papi/infra/scripts/logs
